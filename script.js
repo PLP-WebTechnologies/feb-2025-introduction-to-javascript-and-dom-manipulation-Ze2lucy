1a0b820aca77af0a6c2670d5b1c587a40c2cd7f7
Ze2-lucy <zethulucysitole@gmail.com>
2025-05-04 18:46:38 +0200

    Select elements for dynamic addition/removal functionality

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const dynamicText = document.getElementById("dynamic-text");
 changeTextBtn.addEventListener("click", () => {
   dynamicText.textContent = "The text has been changed!";
 });
+
+const changeStyleBtn = document.getElementById("change-style-btn");
+const styledText = document.getElementById("styled-text");
+
+changeStyleBtn.addEventListener("click", () => {
+  styledText.style.color = "blue";
+  styledText.style.fontSize = "20px";
+  styledText.style.fontWeight = "bold";
+});
